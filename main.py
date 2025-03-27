b052e45f50504cff466e96dc71bbd36ca59c814b
HeadlessWing <wingcue@gmail.com>
2025-03-27 11:48:33 -0700

    added line drawing

--- main.py
+++ main.py
@@ -2,7 +2,17 @@ from  window import  *
 
 def main():
     win = Window(800,600)
+    
+    point_1 = Point(50, 50)
+    point_2 = Point(25, 25)
+    line_1 = Line(point_1, point_2)
+    win.draw_line(line_1, "black")
+    point_3 = Point(75, 10)
+    point_4 = Point(10, 75)
+    line_2 = Line(point_3, point_4)
+    win.draw_line(line_2, "black")
     win.wait_for_close()
+
 
 if __name__ == "__main__":
     main()
